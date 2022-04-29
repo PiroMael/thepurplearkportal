eb1b81ad3ba384c92832c9b8488b260467225522
Mael Piro <piromael46@gmail.com>
2022-04-29 18:51:39 +0200

    post creation initialized

---
+++ app/javascript/components/setCoordonates.js
@@ -0,0 +1,16 @@
+const setposition= () => {
+   
+    let createPostBtn = document.getElementById('new_post');
+
+
+}
+const initsetposition = () => {
+
+    if (document.querySelector('#cross')) {
+        setposition();
+       
+    }
+  }
+  
+  
+  export { initsetposition };
